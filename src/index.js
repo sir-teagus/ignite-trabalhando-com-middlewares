e4edac79f99323d75919323eb8e644ffb3f7eb71
Thiago Souza <teagus@gmail.com>
2021-05-21 15:16:12 -0300

    Challenge solved

--- src/index.js
+++ src/index.js
@@ -10,20 +10,79 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ 'error': 'Username does not exists' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user) {
+    return response.status(400).json({ 'error': 'Username does not exists' });
+  }
+
+  if (!user.pro && user.todos.length === 10) {
+    return response.status(403).json({ 'error': 'Update to a PRO account to have more than 10 todos.' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ 'error': 'Username does not exists' });
+  }
+
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({ 'error': 'Invalid ID' });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({error: 'Todo does not exists'})
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ 'error': 'Username does not exists' });
+  }
+
+  request.user = user;
+
+  return next();
 }
+
+app.get('/users', (request, response) => {
+  return response.json(users);
+});
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
